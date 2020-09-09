17d5188bf2d433c9517ba81a02f18c865b22554d
pijiande <54364557+pijiande@users.noreply.github.com>
2020-09-09 22:16:31 +0800

    Add files via upload

---
+++ Doub_Atten.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def gather(input1, input2):
+    b,c1,h,w = input1.shape
+    _,c2,_,_ = input2.shape
+    input1 = input1.view(b, 1, c1, h*w)
+    input2 = input2.view(b, c2, 1, h*w)
+    temp = input1 * F.softmax(input2, dim=-1)
+
+    return temp.mean(dim=-1)
+
+class DoubleAtten(nn.Module):
+    """
+    A2-Nets: Double Attention Networks. NIPS 2018
+    """
+    def __init__(self,in_c):
+        """
+        :param
+        in_c: 进行注意力refine的特征图的通道数目；
+        原文中的降维和升维没有使用
+        """
+        super(DoubleAtten,self).__init__()
+        self.in_c = in_c
+        """
+        以下对同一输入特征图进行卷积，产生三个尺度相同的特征图，即为文中提到A, B, V
+        """
+        self.convA = nn.Conv2d(in_c,in_c,kernel_size=1)
+        self.convB = nn.Conv2d(in_c,in_c,kernel_size=1)
+        self.convV = nn.Conv2d(in_c,in_c,kernel_size=1)
+    def forward(self,input):
+
+        feature_maps = self.convA(input)
+        atten_map = self.convB(input)
+        b, _, h, w = feature_maps.shape
+
+        feature_maps = feature_maps.view(b, 1, self.in_c, h*w) # 对 A 进行reshape
+        atten_map = atten_map.view(b, self.in_c, 1, h*w)       # 对 B 进行reshape 生成 attention_aps
+        global_descriptors = torch.mean((feature_maps * F.softmax(atten_map, dim=-1)),dim=-1) # 特征图与attention_maps 相乘生成全局特征描述子
+
+        v = self.convV(input)
+        atten_vectors = F.softmax(v.view(b, self.in_c, h*w), dim=-1) # 生成 attention_vectors
+        out = torch.bmm(atten_vectors.permute(0,2,1), global_descriptors).permute(0,2,1) # 注意力向量左乘全局特征描述子
+
+        return out.view(b, _, h, w)
+
+a = torch.randn(size=(4,512,16,16))
+model = DoubleAtten(512)
+a = model(a)
+print(a.shape)
+
+
+
+
+
+
+
+
